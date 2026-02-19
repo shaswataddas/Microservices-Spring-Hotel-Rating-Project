2e69a852a2c3adf7a4bbd89742720981585c4a4a
Shaswata <shaswata.das@cloudkaptan.com>
2026-02-19 13:53:02 +0530

    Update the getAllUser method, Implement Config Server, Implement API Gateway and CircuitBreaker

--- UserService/UserService/src/main/java/com/learnspring/user/service/controller/UserController.java
+++ UserService/UserService/src/main/java/com/learnspring/user/service/controller/UserController.java
@@ -2,6 +2,9 @@ package com.learnspring.user.service.controller;
 
 import com.learnspring.user.service.entities.User;
 import com.learnspring.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import java.util.List;
 public class UserController {
 
     private UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -27,9 +31,22 @@ public class UserController {
 
     //single user get
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //creating ratingHotelFallback method for circuitBreaker
+    public ResponseEntity<User> ratingHotelFallback (String userId, Exception e){
+        logger.info("Fallback method called due to service unavailability");
+        User failedUser = User.builder()
+                .email("dummyuser@yopmail.com")
+                .about("This is a Dummy User for Fault Tolerance")
+                .name("Dummy")
+                .userId("123456789")
+                .build();
+        return ResponseEntity.ok(failedUser);
     }
 
 
