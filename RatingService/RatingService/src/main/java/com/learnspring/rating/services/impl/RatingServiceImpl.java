641e64ceee7d43f0adcd8e444552532bbc94b0e8
Shaswata <shaswata.das@cloudkaptan.com>
2026-02-23 18:06:20 +0530

    Implement Rate Limiter in the Microservice communication.

--- RatingService/RatingService/src/main/java/com/learnspring/rating/services/impl/RatingServiceImpl.java
+++ RatingService/RatingService/src/main/java/com/learnspring/rating/services/impl/RatingServiceImpl.java
@@ -1,9 +1,11 @@
 package com.learnspring.rating.services.impl;
 
+import com.learnspring.rating.entities.Hotel;
 import com.learnspring.rating.entities.Rating;
 import com.learnspring.rating.repositories.RatingRepository;
 import com.learnspring.rating.services.RatingService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.UUID;
@@ -12,9 +14,11 @@ import java.util.UUID;
 public class RatingServiceImpl implements RatingService {
 
     private RatingRepository ratingRepository;
+    private RestTemplate restTemplate;
 
-    public RatingServiceImpl(RatingRepository ratingRepository) {
+    public RatingServiceImpl(RatingRepository ratingRepository, RestTemplate restTemplate) {
         this.ratingRepository = ratingRepository;
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -31,11 +35,44 @@ public class RatingServiceImpl implements RatingService {
 
     @Override
     public List<Rating> getAllByUserId(String userId) {
-        return ratingRepository.findByUserId(userId);
+
+        // Fetch ratings from DB
+        List<Rating> ratings = ratingRepository.findByUserId(userId);
+
+        // Fetch hotel details for each rating
+        ratings.forEach(rating -> {
+
+            Hotel hotel = restTemplate.getForObject(
+                    "http://HOTELSERVICE/hotels/" + rating.getHotelId(),
+                    Hotel.class
+            );
+
+            rating.setHotel(hotel);
+        });
+
+        return ratings;
     }
 
     @Override
     public List<Rating> getAllByHotelId(String hotelId) {
-        return ratingRepository.findByHotelId(hotelId);
+
+        // Fetch ratings from DB
+        List<Rating> ratings = ratingRepository.findByHotelId(hotelId);
+
+        if (ratings.isEmpty()) {
+            return ratings;
+        }
+
+        // Fetch hotel details ONLY ONCE
+        Hotel hotel = restTemplate.getForObject(
+                "http://HOTELSERVICE/hotels/" + hotelId,
+                Hotel.class
+        );
+
+        // Attach hotel to all ratings
+        ratings.forEach(rating -> rating.setHotel(hotel));
+
+        return ratings;
     }
+
 }
