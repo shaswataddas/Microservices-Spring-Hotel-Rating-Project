1c57b35ef6dc10750378632d82969b22c81f05b4
Shaswata <shaswata.das@cloudkaptan.com>
2026-03-18 23:29:17 +0530

    Implement the okta security for other Services as well

---
+++ RatingService/RatingService/src/main/java/com/learnspring/rating/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.learnspring.rating.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        return security.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+
+    }
+}
---
+++ UserService/UserService/src/main/java/com/learnspring/user/service/config/interceptor/FeignClientInterceptor.java
@@ -0,0 +1,24 @@
+package com.learnspring.user.service.config.interceptor;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Autowired
+    private OAuth2AuthorizedClientManager manager;
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+
+        String token = String.valueOf(manager.authorize(OAuth2AuthorizeRequest.withClientRegistrationId("user_service_internal_client").principal("internal").build()).getAccessToken().getTokenValue());
+        requestTemplate.header("Authorization","Bearer "+token);
+    }
+}
